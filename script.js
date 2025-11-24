cf635fd73f117db0f9900cf85b32c0f71528e47c
David Gift <giftdavid189@gmail.com>
2025-11-24 19:38:52 +0100

    updated registration form

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
-DOMContentLoaded
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('registration-form')
+    const feedbackDiv = document.getElementById('form-feedback')
 
-const form = document.getElementById('registration-form')
-const feedbackDiv = document.getElementById('form-feedback')
+    form.addEventListener('submit', function(event) {
+        event.preventDefault()
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault()
+        let isValid = true
+        let messages = []
 
-    let isValid = true
-    let messages = []
+        const username = (document.getElementById('username')).trim()
 
-    const username = document.getElementById('username')
+        if (username.length < 3){
+            isValid = false
+            messages += "Username can't be less than 3 characters"
+        }
 
-    if (username.length < 3){
-        isValid = false
-        messages += "Username can't be less than 3 characters"
-    }
+        const email = document.getElementById('email').trim()
 
-    const email = document.getElementById('email')
+        if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(emailInput.value)){
+            isValid = false
+            messages += "Enter a valid email"
+        }
 
-    if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(emailInput.value)){
-        isValid = false
-        messages += "Enter a valid email"
-    }
+        const password = document.getElementById('password')
 
-    const password = document.getElementById('password')
+        if (password < 8){
+            isValid = false
+            messages += "Password can't be less than 8 characters"
+        }
 
-    if (password < 8){
-        isValid = false
-        messages += "Password can't be less than 8 characters"
-    }
+        feedbackDiv.style.display = "block"
 
-    feedbackDiv.style.display = "block"
+        if (isValid){
+            feedbackDiv.textContent = "Registration successful!"
+        }
 
-    if (isValid){
-        feedbackDiv.textContent = "Registration successful!"
-    }
+        form.submit()
+    })
+});
 
-    form.submit()
-})
